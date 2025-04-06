3e45bd9362caeed0078a4c7c58858ad39383080e
Rudnay Roland <rudnayroland@gmail.com>
2025-04-06 12:31:21 +0200

    default

---
+++ Match/src/test/java/match_tracker/Match/MatchApplicationTests.java
@@ -0,0 +1,13 @@
+package match_tracker.Match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
